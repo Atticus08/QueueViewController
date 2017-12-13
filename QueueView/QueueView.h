584f26e690275849a65b15df96ff9888aab2fd21
Atticus08 <tfritz08@gmail.com>
2017-12-13 12:54:27 -0500

    v0.1 commit

---
+++ QueueView/QueueView.h
@@ -0,0 +1,19 @@
+//
+//  QueueView.h
+//  QueueView
+//
+//  Created by Atticus on 12/13/17.
+//  Copyright © 2017 Atticus08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QueueView.
+FOUNDATION_EXPORT double QueueViewVersionNumber;
+
+//! Project version string for QueueView.
+FOUNDATION_EXPORT const unsigned char QueueViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QueueView/PublicHeader.h>
+
+
